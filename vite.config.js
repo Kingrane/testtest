0e709b36b01e8836baff3987b93c49c9c1fb8967
kingrane <borisovroma55@gmail.com>
2026-02-05 16:57:58 +0300

    Fix base path for Vercel and GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: '/testtest/',
+    base: './',
     build: {
         outDir: 'dist'
     }
